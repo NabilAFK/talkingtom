e9ecf59bc8f8b90a4b43736fbb370a6ad4be64a5
Jaagrav <jaagravseal03@gmail.com>
2020-08-10 19:07:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,20 +3,20 @@ let mic,
   soundFile,
   recording = false,
   sensitivity = 0.5;
-window.onload = function(){
+function setup() {
+  mic = new p5.AudioIn();
+  frameRate(8);
+  recorder = new p5.SoundRecorder();
+  recorder.setInput(mic);
+  soundFile = new p5.SoundFile();
   swal(
     "To use Talking Tom Web, you need to allow microphone to record your voice."
   ).then((e) => {
-    mic = new p5.AudioIn();
     mic.start();
-    recorder = new p5.SoundRecorder();
-    recorder.setInput(mic);
-    soundFile = new p5.SoundFile();
-    setInterval(recurrr,100);
   });
 }
-function recurrr(){
-   if (!soundFile.isPlaying() && !recording) {
+function draw() {
+  if (!soundFile.isPlaying() && !recording) {
     document.querySelector(".mouth").style.animation = "";
     document.querySelector(".stat").innerHTML = "Say something...";
   }
@@ -31,6 +31,7 @@ function recurrr(){
     if (!soundFile.isPlaying() && recording) {
       recorder.stop();
       setTimeout(() => {
+        soundFile.amp(1);
         soundFile.rate(1.4);
         soundFile.play();
         document.querySelector(".mouth").style.animation =
